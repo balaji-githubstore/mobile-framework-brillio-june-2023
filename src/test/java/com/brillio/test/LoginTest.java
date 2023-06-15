9164b56338b6558d084d16c3820f5aa63adb8e82
balaji-githubstore <balaji0017@gmail.com>
2023-06-15 12:22:31 +0530

    Created First Test Method

---
+++ src/test/java/com/brillio/test/LoginTest.java
@@ -0,0 +1,52 @@
+package com.brillio.test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+public class LoginTest {
+
+	@Test
+	public void invalidLoginTest() throws MalformedURLException {
+
+		File file = new File("apps/khan-academy-7-3-2.apk");
+		String appPath = file.getAbsolutePath();
+
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("platformName", "android");
+		caps.setCapability("deviceName", "redmi");
+		caps.setCapability("app", appPath);
+
+		AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Dismiss']")).click();
+
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Sign in']")).click();
+
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Sign in']")).click();
+
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[contains(@text,'address or')]"))
+				.sendKeys("john@gmail.com");
+
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@content-desc='Password']"))
+				.sendKeys("welcome123");
+
+		driver.findElement(AppiumBy.xpath("(//android.widget.TextView[@text='Sign in'])[2]")).click();
+
+		String actualError = driver.findElement(AppiumBy.xpath("//*[contains(@text,'issue')]")).getText();
+		
+		Assert.assertEquals(actualError, "There was an issue signing in");
+
+	}
+
+}
