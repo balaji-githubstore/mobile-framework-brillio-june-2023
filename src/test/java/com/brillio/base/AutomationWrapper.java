24981c35040a4d255b73d5c53649a7664f49bc5e
balaji-githubstore <balaji0017@gmail.com>
2023-06-16 12:49:15 +0530

    Framework designed -android local and cloud execution - properties, data driven, parallel executed

--- src/test/java/com/brillio/base/AutomationWrapper.java
+++ src/test/java/com/brillio/base/AutomationWrapper.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.openqa.selenium.By;
@@ -13,6 +14,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import com.brillio.utilities.PropUtils;
 
@@ -24,65 +27,96 @@ import io.appium.java_client.service.local.flags.GeneralServerFlag;
 public class AutomationWrapper {
 
 	protected AndroidDriver driver;
-	private AppiumDriverLocalService service;
-	Map<Object, Object> propEnvMap;
+	private static AppiumDriverLocalService service;
+	private static Map<Object, Object> propEnvMap;
 
 	// server setup
 	// cloud device
 	// record the session as video (.mp4)
 	// extent report
-	
+
 	@BeforeSuite
-	public void startSession() throws IOException
-	{
-		propEnvMap=PropUtils.getPropertiesIntoMap("test-data/data.properties");
-		
-		if(service == null && !service.isRunning())
+	public void startSession() throws IOException {
+		if(propEnvMap==null)
 		{
-			AppiumServiceBuilder builder=new AppiumServiceBuilder();
-			builder.usingAnyFreePort();
-			builder.withLogFile(new File("logs/appiumlog.log"));
-			builder.withArgument(GeneralServerFlag.BASEPATH,"/wd/hub");
-			builder.withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"));
-			service=AppiumDriverLocalService.buildService(builder);
-			
-			service.start();
+			propEnvMap = PropUtils.getPropertiesIntoMap("test-data/data.properties");
 		}
 		
-		//pass the service on the AndroidDriver object
+
+		if (propEnvMap.get("runenv").toString().equalsIgnoreCase("local") && service == null) {
+			AppiumServiceBuilder builder = new AppiumServiceBuilder();
+			builder.usingAnyFreePort();
+			builder.withLogFile(new File(propEnvMap.get("logLocation").toString()));
+			builder.withArgument(GeneralServerFlag.BASEPATH, "/wd/hub");
+			builder.withAppiumJS(new File(propEnvMap.get("appiumJSpath").toString()));
+			service = AppiumDriverLocalService.buildService(builder);
+
+			service.start();
+		}
+		// pass the service on the AndroidDriver object
 	}
-	
+
 	@AfterSuite
-	public void endSession()
-	{
-		if(service.isRunning())
-		{
+	public void endSession() {
+		if (service != null && service.isRunning()) {
 			service.stop();
 		}
 	}
 
 	@BeforeMethod
-	public void setup() throws MalformedURLException {
-
-		File file = new File("apps/khan-academy-7-3-2.apk");
-		String appPath = file.getAbsolutePath();
+	@Parameters({"deviceName","platformVersion"})
+	public void setup(@Optional("None") String deviceName,@Optional("None") String platformVersion) throws MalformedURLException {
 
 		DesiredCapabilities caps = new DesiredCapabilities();
-		caps.setCapability("platformName", "android");
-		caps.setCapability("deviceName", "redmi");
-		caps.setCapability("app", appPath);
-		caps.setCapability("autoLaunch", "false");
-
-		driver = new AndroidDriver(service,caps);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-		driver.activateApp("org.khanacademy.android");
-
-		if (driver.findElements(By.xpath("//*[@text='Allow']")).size() > 0) {
-			driver.findElement(By.xpath("//*[@text='Allow']")).click();
+		caps.setCapability("platformName", propEnvMap.get("platformName").toString());
+		
+		if(deviceName.equalsIgnoreCase("none"))
+		{
+			caps.setCapability("deviceName", propEnvMap.get("deviceName").toString());
+			caps.setCapability("platformVersion", propEnvMap.get("platformVersion").toString());
 		}
-	}
+		else
+		{
+			caps.setCapability("deviceName",deviceName);
+			caps.setCapability("platformVersion", platformVersion);
+		}
+		
+		
 
+		if (propEnvMap.get("runenv").toString().equalsIgnoreCase("local")) {
+			String appPath = new File(propEnvMap.get("app").toString()).getAbsolutePath();
+
+			caps.setCapability("autoLaunch", "false");
+			caps.setCapability("app", appPath);
+
+			driver = new AndroidDriver(service, caps);
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+			driver.activateApp("org.khanacademy.android");
+
+			if (driver.findElements(By.xpath("//*[@text='Allow']")).size() > 0) {
+				driver.findElement(By.xpath("//*[@text='Allow']")).click();
+			}
+		} else {
+			
+			caps.setCapability("app", propEnvMap.get("appCloud").toString());
+
+			Map<String, String> bstackDetails = new HashMap<String, String>();
+
+			bstackDetails.put("projectName", propEnvMap.get("projectName").toString());
+			bstackDetails.put("buildName", propEnvMap.get("buildName").toString());
+			bstackDetails.put("sessionName", propEnvMap.get("sessionName").toString());
+
+			bstackDetails.put("userName", propEnvMap.get("userName").toString());
+			bstackDetails.put("accessKey", propEnvMap.get("accessKey").toString());
+
+			caps.setCapability("bstack:options", bstackDetails);
+
+			driver = new AndroidDriver(new URL(propEnvMap.get("browserstackUrl").toString()), caps);
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		}
+
+	}
 
 	@AfterMethod
 	public void teardown() {
--- src/test/java/com/brillio/test/SignUpTest.java
+++ src/test/java/com/brillio/test/SignUpTest.java
@@ -5,16 +5,26 @@ import org.testng.annotations.Test;
 import com.brillio.base.AutomationWrapper;
 
 import io.appium.java_client.AppiumBy;
+
 /**
  * This class contains test methods of login scenario
  */
-public class SignUpTest extends AutomationWrapper{
-	
+public class SignUpTest extends AutomationWrapper {
+
 	@Test
-	public void validRegisterTest()
-	{
+	public void validRegisterTest() {
 		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Dismiss']")).click();
-		//test method to valid successful registration
+
+		driver.findElement(AppiumBy.xpath("//android.widget.ImageView[@content-desc='Settings']")).click();
+
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Sign in']")).click();
+
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Sign up with email']")).click();
+
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@text='First name']")).sendKeys("john");
+
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[@text='Last name']")).sendKeys("wick");
+
 		System.out.println("valid registration");
 	}
 
