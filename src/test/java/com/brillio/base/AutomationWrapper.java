9c2f802db7c9f2c135a9dbed8db5fcceb048cd40
balaji-githubstore <balaji0017@gmail.com>
2023-06-16 10:12:23 +0530

    Handled Notification pop up on device launch

--- src/test/java/com/brillio/base/AutomationWrapper.java
+++ src/test/java/com/brillio/base/AutomationWrapper.java
@@ -5,6 +5,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,14 +13,13 @@ import org.testng.annotations.BeforeMethod;
 import io.appium.java_client.android.AndroidDriver;
 
 public class AutomationWrapper {
-	
+
 	protected AndroidDriver driver;
-	
-	
-	//server setup
-	//cloud device
-	//record the session as video (.mp4) 
-	//extent report
+
+	// server setup
+	// cloud device
+	// record the session as video (.mp4)
+	// extent report
 
 	@BeforeMethod
 	public void setup() throws MalformedURLException {
@@ -31,9 +31,16 @@ public class AutomationWrapper {
 		caps.setCapability("platformName", "android");
 		caps.setCapability("deviceName", "redmi");
 		caps.setCapability("app", appPath);
+		caps.setCapability("autoLaunch", "false");
 
 		driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.activateApp("org.khanacademy.android");
+
+		if (driver.findElements(By.xpath("//*[@text='Allow']")).size() > 0) {
+			driver.findElement(By.xpath("//*[@text='Allow']")).click();
+		}
 	}
 
 	@AfterMethod
